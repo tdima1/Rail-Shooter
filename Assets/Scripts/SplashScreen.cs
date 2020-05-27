1c674f5ec967e418ffc5116aacf53b6b42a88b56
Dima Tiberiu <dima.tiberiu1@gmail.com>
2020-05-27 22:38:10 +0300

    Added complex controls for starship. Added tilt and rotation according to position on screen.

--- Assets/Scripts/SplashScreen.cs
+++ Assets/Scripts/SplashScreen.cs
@@ -1,18 +1,28 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class SplashScreen : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   private float _rotationAngle = 0;
+   
+   [SerializeField] public AudioSource _musicPlayer;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   // Start is called before the first frame update
+   void Start()
+   {
+      DontDestroyOnLoad(_musicPlayer);
+      Invoke("LoadFirstScene", 3f);
+   }
+
+   void Update()
+   {
+      _rotationAngle += 5f * Time.deltaTime;
+      transform.rotation = Quaternion.Euler(_rotationAngle * new Vector3(0, 1, 1));
+   }
+
+   private void LoadFirstScene()
+   {
+      SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+   }
 }
---
+++ Assets/Scripts/StarshipControls.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class StarshipControls : MonoBehaviour
+{
+   private float _horizontalInput;
+   private float _verticalInput;
+   [Tooltip("In ms^-1")][SerializeField] private float _shipStrafingSpeed = 5f;
+   [Tooltip("In ms^-1")] [SerializeField] private float _shipAscendingSpeed = 5f;
+   [Tooltip("In meters")] [SerializeField] private float _shipMovementRangeHorizontal = 5f;
+   [Tooltip("In meters")] [SerializeField] private float _shipMovementRangeVertical = 3f;
+
+   [SerializeField] private float _pitchPositionFactor = -5f;
+   [SerializeField] private float _pitchTiltFactor = -20f;
+
+   [SerializeField] private float _yawPositionFactor = 5f;
+
+   [SerializeField] private float _rollPositionFactor = 5f;
+   [SerializeField] private float _rollTiltFactor = -30f;
+
+   // Start is called before the first frame update
+   void Start()
+   {
+
+   }
+
+   // Update is called once per frame
+   void Update()
+   {
+      HandleStarshipTranslation();
+      HandleStarshipRotation();
+   }
+
+   private void HandleStarshipRotation()
+   {
+      float pitchFromPosition = transform.localPosition.y * _pitchPositionFactor;
+      float pitchFromTilt = _verticalInput * _pitchTiltFactor;
+      float pitch = pitchFromPosition + pitchFromTilt;
+
+      float yaw = transform.localPosition.x * _yawPositionFactor;
+
+      float rollFromPosition = transform.localPosition.x * _rollPositionFactor;
+      float rollFromTilt = _horizontalInput * _rollTiltFactor;
+      float roll = rollFromPosition + rollFromTilt;
+
+      transform.localRotation = Quaternion.Euler(new Vector3(pitch, yaw, roll));
+   }
+
+   private void HandleStarshipTranslation()
+   {
+      _horizontalInput = Input.GetAxis("Horizontal");
+      _verticalInput = Input.GetAxis("Vertical");
+      float shipOffsetHorizontal = _horizontalInput * Time.deltaTime * _shipStrafingSpeed;
+      float rawShipOffsetHorizontal = transform.localPosition.x + shipOffsetHorizontal;
+      float clampedShipOffsetHorizontal = Mathf.Clamp(rawShipOffsetHorizontal, -_shipMovementRangeHorizontal, _shipMovementRangeHorizontal);
+
+      float shipOffsetVertical = _verticalInput * Time.deltaTime * _shipAscendingSpeed;
+      float rawShipOffsetVertical = transform.localPosition.y + shipOffsetVertical;
+      float clampedShipOffsetVertical = Mathf.Clamp(rawShipOffsetVertical, -_shipMovementRangeVertical, _shipMovementRangeVertical);
+
+      transform.localPosition = new Vector3(clampedShipOffsetHorizontal, clampedShipOffsetVertical, transform.localPosition.z);
+   }
+}
